2ee60bb91a4e35c5beafbd4616707f2f31432edb
Ashlyn <ashlynchor15@gmail.com>
2024-02-06 10:11:43 +0800

    update models.py

--- GISFirstDjango/models.py
+++ GISFirstDjango/models.py
@@ -4,10 +4,9 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-
-class Students(models.Model):
-    ...
+    age = models.IntegerField(blank=False, null=True)
 
 class TeachingGroup(models.Model):
-    student = models.ForeignKey(Students, on_delete = models.CASCADE,)
-#    students = models.OneToOneField(Person, null=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=30, null=True)
+    students = models.ForeignKey(Person, null=True, on_delete=models.SET_NULL)
+    year_group = models.IntegerField(blank=False, null=True)
