94b53e449ded1c58b12cd764e719a66a8bcb6244
Ashlyn <ashlynchor15@gmail.com>
2024-02-06 10:09:35 +0800

    update views.py

--- GISFirstDjango/views.py
+++ GISFirstDjango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Person
 
 def splash(request):
-    name = 'Billy'
+    name = 'Minnow'
     people = Person.objects.all()
     debug_people = list(people)
     return render(request, "splash.html", {'name': name})
